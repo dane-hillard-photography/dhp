f02eca552c65cf01ed7ed4912637041fc3b68e7b
Dane Hillard <dane@danehillard.com>
2013-12-30 22:19:46 -0500

    Adding most recent photos to homepage

--- home/views.py
+++ home/views.py
@@ -1,4 +1,14 @@
 from django.views import generic
+from django.db.models import Q
+from django.utils import timezone
+
+from photography.models import Photograph
 
 class IndexView(generic.TemplateView):
   template_name = 'home/base.html'
+
+  def get_context_data(self, **kwargs):
+	  context = super(IndexView, self).get_context_data(**kwargs)
+	  context['latest_photos'] = Photograph.objects.filter(
+			  Q(published_date__lte=timezone.now()) & (Q(public=True) | Q(user_id=self.request.user.id))).order_by('-published_date')[:5]
+	  return context
