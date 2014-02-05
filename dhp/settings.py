890cd12d97445388603d92dfe168702c8a09e744
Dane Hillard <dane@danehillard.com>
2014-02-05 17:56:29 -0500

    Adding south for schema migrations

--- dhp/settings.py
+++ dhp/settings.py
@@ -142,6 +142,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+	'south',
     'photography',
     'home',
     'about',
