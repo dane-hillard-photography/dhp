6b357d4f08b06f3649856fcfc065efdd399c884b
Dane Hillard <dane.hillard@thermofisher.com>
2014-06-25 15:56:52 -0400

    Updating fabfile to modularize some functions, make things more easily reusable, and allow for more granular control of what gets run.

--- fabfile.py
+++ fabfile.py
@@ -1,50 +1,97 @@
 from fabric.api import *
 
-@task(alias='loc')
+@task(alias='l')
 def localhost():
+    """Sets up environment for use on localhost"""
     env.run = local
     env.project_venv = 'dane-dhp'
+    env.project_path = '/Users/danehillard/me/sites/dhp'
 
-@task(alias='rem')
+@task(alias='r')
 def remote():
-    env.hosts = ['danehillard.com']
-    env.run = run
-    env.user = 'www'
+    """Sets up environment for use on remote machine"""
+    env.host = 'www@danehillard.com'
     env.key_filename = '/Users/danehillard/.ssh/dhillard.pem'
+    env.run = run
     env.project_venv = 'dhp'
     env.project_path = 'dhp'
 
-def manage(args):
+@task
+def manage(*args):
+    """Runs `./manage.py {args}`"""
     with prefix('workon {venv}'.format(venv=env.project_venv)):
-        env.run('./manage.py {args}'.format(args=args))
-
-def compile_sass():
-    run('for file in `find . -name "*.sass"`; do sass $file:${file%.sass}.css; done')
-
-@task(alias='static')
-def update_static_files():
-    compile_sass()
-    manage('collectstatic --noinput -i *.sass')
-
-@task(alias='db')
-def update_database():
-    manage('syncdb --noinput')
-    manage('migrate --noinput')
-
-@task(alias='stage')
-def stage_commit_push_changes():
-    local('git add -p')
-    local('git commit')
-    local('git push')
+        env.run('./manage.py {args}'.format(args=' '.join(args)))
 
 @task
-def reload_wsgi():
-    run('touch apache/django.wsgi')
+def sass(search_path='.'):
+    """Compiles SASS files in place as CSS."""
+    env.run('for file in `find {path} -name "*.sass"`; do sass $file:${{file%.sass}}.css; done'.format(
+        path=search_path
+    ))
 
-@task(alias='dep')
+@task
+def static():
+    """Runs 'sass' and 'collectstatic' tasks"""
+    sass()
+
+@task
+def collectstatic():
+    """Runs `./manage.py collectstatic --noinput -i *.sass`"""
+    manage('collectstatic --noinput -i *.sass')
+
+@task
+def syncdb():
+    """Runs `./manage.py syncdb --noinput`"""
+    manage('syncdb --noinput')
+
+@task
+def migrate(*args):
+    """Runs `./manage.py migrate --noinput {args}`"""
+    manage('migrate --noinput {args}'.format(args=' '.join(args)))
+
+@task
+def db():
+    """Runs 'syncdb' and 'migrate' tasks"""
+    syncdb()
+    migrate()
+
+@task
+def stage():
+    """Prompts user about each chunk of changed code, adds them to the commit, then commits and pushes them."""
+    git_add('-p')
+    git_commit()
+    git_push()
+
+@task
+def reload():
+    """Touches WSGI file to reload the app in the webserver"""
+    env.run('touch apache/django.wsgi')
+
+@task
+def git_pull(repository='', branch=''):
+    """Runs `git pull {repo} {branch}`"""
+    env.run('git pull {repo} {br}'.format(repo=repository, br=branch))
+
+@task
+def git_push(repository='', branch=''):
+    """Runs `git push {repo} {branch}`"""
+    env.run('git push {repo} {br}'.format(repo=repository, br=branch))
+
+@task
+def git_commit(*args):
+    """Runs `git commit {args}`"""
+    env.run('git commit {args}'.format(args=' '.join(args)))
+
+@task
+def git_add(*args):
+    """Runs `git commit {args}`"""
+    env.run('git commit {args}'.format(args=' '.join(args)))
+
+@task
 def deploy():
+    """Pulls changes from master, updates the database, updates static files, and tells the webserver to reload."""
     with cd(env.project_path):
-        run('git pull')
-        update_database()
-        update_static_files()
-        reload_wsgi()
+        git_pull()
+        db()
+        static()
+        reload()
