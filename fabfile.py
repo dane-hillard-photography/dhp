1374af7e6d6a59da9a8f7e5608fe46b55a0d0960
Dane Hillard <dane.hillard@thermofisher.com>
2014-06-24 20:19:18 -0400

    Adding other files needed from previous commit that were untracked.

---
+++ fabfile.py
@@ -0,0 +1,46 @@
+from fabric.api import *
+
+@task(alias='loc')
+def localhost():
+    env.run = local
+    env.project_venv = 'dane-dhp'
+
+@task(alias='rem')
+def remote():
+    env.hosts = ['danehillard.com']
+    env.run = run
+    env.user = 'www'
+    env.key_filename = '/Users/danehillard/.ssh/dhillard.pem'
+    env.project_venv = 'dhp'
+    env.project_path = 'dhp'
+
+def manage(args):
+    with prefix('source /usr/local/bin/virtualenvwrapper.sh; workon {venv}'.format(venv=env.project_venv)):
+        env.run('./manage.py {args}'.format(args=args))
+
+@task(alias='cs')
+def collectstatic():
+    manage('collectstatic --noinput -i *.sass')
+
+@task(alias='db')
+def update_database():
+    manage('syncdb --noinput')
+    manage('migrate --noinput')
+
+@task(alias='stage')
+def stage_commit_push_changes():
+    local('git add -p')
+    local('git commit')
+    local('git push')
+
+@task
+def reload_wsgi():
+    run('touch apache/django.wsgi')
+
+@task(alias='dep')
+def deploy():
+    with cd(env.project_path):
+        run('git pull')
+        update_database()
+        collectstatic()
+        reload_wsgi()
