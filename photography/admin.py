e01efef1b058f5525aed4289b0a11797c322e06e
Dane Hillard <dane.hillard@thermofisher.com>
2014-06-29 14:24:36 -0400

    Breaking out thumbnail creation into its own function. Changing implementation of sort_order to happen in AlbumAdmin.

--- photography/admin.py
+++ photography/admin.py
@@ -25,6 +25,27 @@ class AlbumAdmin(admin.ModelAdmin):
     list_display = ('title', 'sort_order', 'published_date', 'public', 'user', 'uuid')
     date_hierarcy = 'published_date'
 
+    def save_model(self, request, obj, form, change):
+        if 'sort_order' in form.changed_data:
+            new_order = form.cleaned_data['sort_order']
+
+            current_order = Album.objects.get(pk=obj.id).sort_order
+            existing_album = Album.objects.get(sort_order=new_order)
+
+            if existing_album:
+                if new_order > current_order:
+                    albums = Album.objects.filter(sort_order__lte=new_order, sort_order__gt=current_order)
+                    for album in albums:
+                        album.sort_order -= 1
+                        super(Album, album).save()
+                elif new_order < current_order:
+                    albums = Album.objects.filter(sort_order__lt=current_order, sort_order__gte=new_order).reverse()
+                    for album in albums:
+                        album.sort_order += 1
+                        super(Album, album).save()
+
+        super(AlbumAdmin, self).save_model(request, obj, form, change)
+
 class TagAdmin(admin.ModelAdmin):
     pass
 
--- photography/models.py
+++ photography/models.py
@@ -1,15 +1,16 @@
 import os
 import uuid
 import datetime
-from PIL import Image as PImage
-from PIL import ImageOps as PImageOps
 from tempfile import *
 
+from PIL import Image as PImage
+from PIL import ImageOps as PImageOps
+
 from django.db import models
-from django.contrib.auth.models import User
-from django.core.files import File
-from django.core.urlresolvers import reverse
 from django.db.models import Max
+from django.core.files import File
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
 from dhp.settings import MEDIA_ROOT
 
@@ -24,17 +25,17 @@ def max_sort_order():
 
 def get_file_path(instance, filename):
     ext = filename.split('.')[-1]
-    save_filename = "%s.%s" % (instance.uuid, ext)
+    save_filename = '{uuid}.{ext}'.format(uuid=instance.uuid, ext=ext)
     return os.path.join(instance.directory, save_filename)
 
 class Album(models.Model):
     class Meta:
         ordering = ['sort_order']
 
-    published_date = models.DateTimeField(default=datetime.datetime.now)
     title = models.CharField(max_length=255)
+    uuid = models.CharField('UUID', max_length=36, unique=True, default=generate_uuid, editable=False)
     public = models.BooleanField(default=True)
-    uuid = models.CharField("UUID", max_length=36, unique=True, default=generate_uuid, editable=False)
+    published_date = models.DateTimeField(default=datetime.datetime.now)
     sort_order = models.IntegerField(blank=True, null=True, default=max_sort_order)
     user = models.ForeignKey(User, blank=True, null=True, default=User.objects.get(username='dane').id)
 
@@ -43,34 +44,6 @@ class Album(models.Model):
 
     def get_absolute_url(self):
         return reverse('photography:album', kwargs={'album_id': self.uuid})
-
-    def save(self, *args, **kwargs):
-        current_order = 0
-        new_order = 0
-
-        if self.pk is not None:
-            current = Album.objects.get(pk=self.pk)
-            current_order = current.sort_order
-            new_order = self.sort_order
-
-        if current_order != new_order:
-            existing_album = Album.objects.filter(sort_order=new_order)
-
-            if existing_album:
-                Album.objects.filter(pk=self.pk).update(sort_order=None)
-
-                if current_order < new_order:
-                    albums = Album.objects.filter(sort_order__gt=current_order, sort_order__lte=new_order)
-                    for album in albums:
-                        album.sort_order -= 1
-                        super(Album, album).save()
-                elif current_order > new_order:
-                    albums = Album.objects.filter(sort_order__lt=current_order, sort_order__gte=new_order).reverse()
-                    for album in albums:
-                        album.sort_order += 1
-                        super(Album, album).save()
-
-        super(Album, self).save(*args, **kwargs)
 
 class Photograph(models.Model):
     class Meta:
@@ -82,15 +55,15 @@ class Photograph(models.Model):
         ('S', 'Square'),
     )
   
-    directory = "images/uncropped"
+    directory = os.path.join('images', 'uncropped')
 
-    published_date = models.DateTimeField(default=datetime.datetime.now)
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
+    album = models.ForeignKey(Album, blank=True, null=True, on_delete=models.SET_NULL)
     public = models.BooleanField(default=True)
-    album = models.ForeignKey(Album, blank=True, null=True)
     orientation = models.CharField(max_length=1, choices=ORIENTATION_CHOICES, editable=False)
-    uuid = models.CharField("UUID", max_length=36, unique=True, default=generate_uuid, editable=False)
+    uuid = models.CharField('UUID', max_length=36, unique=True, default=generate_uuid, editable=False)
+    published_date = models.DateTimeField(default=datetime.datetime.now)
 
     user = models.ForeignKey(User, blank=True, null=True, default=User.objects.get(username='dane').id)
 
@@ -105,57 +78,53 @@ class Photograph(models.Model):
     sq_height = models.IntegerField(blank=True, null=True)
     sq_width = models.IntegerField(blank=True, null=True)
 
-    image = models.ImageField(upload_to=get_file_path, height_field="height", width_field="width")
-    thumbnail_large = models.ImageField(upload_to="images/large", blank=True, null=True, height_field="l_height", width_field="l_width")
-    thumbnail_medium = models.ImageField(upload_to="images/medium", blank=True, null=True, height_field="m_height", width_field="m_width")
-    thumbnail_small = models.ImageField(upload_to="images/small", blank=True, null=True, height_field="sm_height", width_field="sm_width")
-    thumbnail_square = models.ImageField(upload_to="images/square", blank=True, null=True, height_field="sq_height", width_field="sq_width")
+    image = models.ImageField(upload_to=get_file_path, height_field='height', width_field='width')
+    
+    thumbnail_large = models.ImageField(
+        upload_to='images/large', blank=True, null=True, height_field='l_height', width_field='l_width')
+    thumbnail_medium = models.ImageField(
+        upload_to='images/medium', blank=True, null=True, height_field='m_height', width_field='m_width')
+    thumbnail_small = models.ImageField(
+        upload_to='images/small', blank=True, null=True, height_field='sm_height', width_field='sm_width')
+    thumbnail_square = models.ImageField(
+        upload_to='images/square', blank=True, null=True, height_field='sq_height', width_field='sq_width')
 
     def get_absolute_url(self):
         from django.core.urlresolvers import reverse
         return reverse('photography:photo', kwargs={'photo_id': self.uuid})
-
-    def save(self, *args, **kwargs):
-        super(Photograph, self).save(*args, **kwargs)
-        im = PImage.open(os.path.join(MEDIA_ROOT, self.image.name))
-        width, height = im.size
-        ratio_divisor = height
-        thumbnail_size = (250, 250)
+    
+    def create_thumbnail(self, original_image, new_image, max_size):
+        width, height = original_image.size
+        ratio_divisor = height if height > width else width
+        ratio = float(max_size) / ratio_divisor
 
         if width > height:
             self.orientation = "L"
-            ratio_divisor = width
         elif width < height:
             self.orientation = "P"
         else:
             self.orientation = "S"
 
-        thumb_square = PImageOps.fit(im, thumbnail_size, PImage.ANTIALIAS)
+        original_image.thumbnail((int(width * ratio), int(height * ratio)), PImage.ANTIALIAS)
         tf = NamedTemporaryFile()
-        thumb_square.save(tf.name, im.format, quality=100)
+        original_image.save(tf.name, original_image.format, quality=100)
+        new_image.save(self.image.name, File(open(tf.name)), save=False)
+        tf.close()
+
+    def save(self, *args, **kwargs):
+        super(Photograph, self).save(*args, **kwargs)
+        image = PImage.open(os.path.join(MEDIA_ROOT, self.image.name))
+        
+        thumbnail_size = (250, 250)
+        thumb_square = PImageOps.fit(image, thumbnail_size, PImage.ANTIALIAS)
+        tf = NamedTemporaryFile()
+        thumb_square.save(tf.name, image.format, quality=100)
         self.thumbnail_square.save(self.image.name, File(open(tf.name)), save=False)
         tf.close()
       
-        ratio = 800.0 / ratio_divisor
-        im.thumbnail((int(width * ratio), int(height * ratio)), PImage.ANTIALIAS)
-        tf = NamedTemporaryFile()
-        im.save(tf.name, im.format, quality=100)
-        self.thumbnail_large.save(self.image.name, File(open(tf.name)), save=False)
-        tf.close()
-
-        ratio = 500.0 / ratio_divisor
-        im.thumbnail((int(width * ratio), int(height * ratio)), PImage.ANTIALIAS)
-        tf = NamedTemporaryFile()
-        im.save(tf.name, im.format, quality=100)
-        self.thumbnail_medium.save(self.image.name, File(open(tf.name)), save=False)
-        tf.close()
-
-        ratio = 200.0 / ratio_divisor
-        im.thumbnail((int(width * ratio), int(height * ratio)), PImage.ANTIALIAS)
-        tf = NamedTemporaryFile()
-        im.save(tf.name, im.format, quality=100)
-        self.thumbnail_small.save(self.image.name, File(open(tf.name)), save=False)
-        tf.close()
+        self.create_thumbnail(image, self.thumbnail_large, 800)
+        self.create_thumbnail(image, self.thumbnail_medium, 500)
+        self.create_thumbnail(image, self.thumbnail_small, 200)
 
         super(Photograph, self).save(*args, **kwargs)
 
