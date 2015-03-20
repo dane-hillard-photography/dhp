8d1303a163eb76e52b45d4d80b2893942d9f8be7
Dane Hillard <dane@danehillard.com>
2015-03-19 23:50:31 -0400

    Only show published posts

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,6 @@
-from django.http import HttpResponse
 from django.views.generic import View
 from django.template import RequestContext
-from django.shortcuts import get_object_or_404
+from django.http import HttpResponse, Http404
 from django.template.loader import get_template_from_string, render_to_string
 
 from blog.models import Post
@@ -9,7 +8,12 @@ from blog.models import Post
 
 class PostView(View):
     def get(self, request, *args, **kwargs):
-        post = get_object_or_404(Post, slug=kwargs.get('slug'))
+        matching_posts = Post.objects.filter(slug=kwargs.get('slug')).filter(published=True)
+        if len(matching_posts) != 1:
+            raise Http404('No published post with slug \'{}\' was found'.format(kwargs.get('slug')))
+        else:
+            post = matching_posts[0]
+
         template_string = '{% extends \'__base.html\' %}{% load snippets %}{% block content %}' + post.body + '{% endblock %}'
         template = get_template_from_string(template_string)
         return HttpResponse(template.render(RequestContext(request)))
