10d2fecd340e0278a77501aa8ba0958766b0744e
Dane Hillard <github@danehillard.com>
2015-07-22 17:17:10 -0400

    Add GA events to blog posts

---
+++ assets/js/blog-post.js
@@ -0,0 +1,18 @@
+$('#feedlyFollow').on('click', function(){
+    ga('send', {
+        'hitType': 'event',
+        'eventCategory': 'button',
+        'eventAction': 'click',
+        'eventLabel': 'feedly follow',
+        'eventValue': 1
+    });
+});
+
+$('#pinterest-pin').on('click', function(){
+    ga('send', {
+        'hitType': 'event',          
+        'eventCategory': 'button',   
+        'eventAction': 'click',      
+        'eventLabel': 'pinterest pin',
+        'eventValue': 1
+    });
