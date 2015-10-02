350a022182116197b39f7f4917e0002dd71cab8a
Dane Hillard <dane@danehillard.com>
2015-10-02 13:05:55 -0700

    Add logging when email sending fails

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.views import generic
 from django.template import Context, loader
 from django.core.urlresolvers import reverse_lazy
@@ -14,18 +16,31 @@ class ContactFormView(FormView):
     form_class = ContactForm
     success_url = reverse_lazy('contact:submit')
 
-    def form_valid(self, form):
+    def send_email(self, source=None, to_addresses=None, reply_addresses=None, subject=None, body=None, format=None):
         conn = ses.connect_to_region(
             'us-east-1',
             aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
             aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY
         )
 
+        result = conn.send_email(
+            source=source,
+            subject=subject,
+            body=body,
+            to_addresses=to_addresses,
+            reply_addresses=reply_addresses,
+            format=format,
+        )
+
+        if 'ErrorResponse' in result:
+            logging.error(result.get('ErrorResponse', {}).get('Error', {}).get('Message',''))
+
+    def form_valid(self, form):
         contact_name = form.cleaned_data.get('name', '')
         contact_first_name = contact_name.split(' ')[0] if contact_name else ''
         contact_email = form.cleaned_data.get('email')
 
-        conn.send_email(
+        self.send_email(
             source='{} <no-reply@danehillard.com>'.format(contact_name),
             subject=form.cleaned_data.get('subject'),
             body='{}\n\n-{}\n{}'.format(form.cleaned_data.get('message'), contact_name, form.cleaned_data.get('phone')),
@@ -34,7 +49,7 @@ class ContactFormView(FormView):
             format='text'
         )
 
-        conn.send_email(
+        self.send_email(
             source='Dane Hillard Photography <no-reply@danehillard.com>',
             subject='Thank you for contacting dHP!',
             body=loader.get_template('contact/contact_thank_you.html').render(Context({'name': contact_first_name})).strip(),
