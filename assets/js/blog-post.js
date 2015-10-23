80119baf5db55190505035a2d7d2ebdc395cb7cf
Dane Hillard <dane@danehillard.com>
2015-10-22 20:53:09 -0400

    Add event tracking for post reads and link clicks

--- assets/js/blog-post.js
+++ assets/js/blog-post.js
@@ -10,10 +10,33 @@ $('#feedlyFollow').on('click', function(){
 
 $('#pinterest-pin').on('click', function(){
     ga('send', {
-        'hitType': 'event',          
-        'eventCategory': 'button',   
-        'eventAction': 'click',      
+        'hitType': 'event',
+        'eventCategory': 'button',
+        'eventAction': 'click',
         'eventLabel': 'pinterest pin',
         'eventValue': 1
     });
+});
+
+$('.external-site a').on('click', function(){
+    ga('send', {
+        'hitType': 'event',
+        'eventCategory': 'button',
+        'eventAction': 'click',
+        'eventLabel': $(this).attr('title'),
+        'eventValue': 1
+    });
+
+$(window).on("scroll", function() {
+    var scrollHeight = $(document).height();
+    var scrollPosition = $(window).height() + $(window).scrollTop();
+    if ((scrollHeight - scrollPosition) / scrollHeight === 0) {
+        ga('send', {
+            'hitType': 'event',
+            'eventCategory': 'post',
+            'eventAction': 'read',
+            'eventLabel': 'post read',
+            'eventValue': 1
+        });
+    }
 });
