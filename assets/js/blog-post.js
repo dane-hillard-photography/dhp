798ac53274d0eb39ee3c43f866f71e2bc76e4a4d
Dane Hillard <github@danehillard.com>
2015-10-26 09:24:47 -0400

    Add document.ready around javascript

--- assets/js/blog-post.js
+++ assets/js/blog-post.js
@@ -1,43 +1,45 @@
-$('#feedlyFollow').on('click', function(){
-    ga('send', {
-        'hitType': 'event',
-        'eventCategory': 'button',
-        'eventAction': 'click',
-        'eventLabel': 'feedly follow',
-        'eventValue': 1
-    });
-});
-
-$('#pinterest-pin').on('click', function(){
-    ga('send', {
-        'hitType': 'event',
-        'eventCategory': 'button',
-        'eventAction': 'click',
-        'eventLabel': 'pinterest pin',
-        'eventValue': 1
-    });
-});
-
-$('.external-site a').on('click', function(){
-    ga('send', {
-        'hitType': 'event',
-        'eventCategory': 'button',
-        'eventAction': 'click',
-        'eventLabel': $(this).attr('title'),
-        'eventValue': 1
-    });
-});
-
-$(window).on("scroll", function() {
-    var scrollHeight = $(document).height();
-    var scrollPosition = $(window).height() + $(window).scrollTop();
-    if ((scrollHeight - scrollPosition) / scrollHeight === 0) {
+$(function(){
+    $('#feedlyFollow').on('click', function(){
         ga('send', {
             'hitType': 'event',
-            'eventCategory': 'post',
-            'eventAction': 'read',
-            'eventLabel': 'post read',
+            'eventCategory': 'button',
+            'eventAction': 'click',
+            'eventLabel': 'feedly follow',
             'eventValue': 1
         });
-    }
+    });
+
+    $('#pinterest-pin').on('click', function(){
+        ga('send', {
+            'hitType': 'event',
+            'eventCategory': 'button',
+            'eventAction': 'click',
+            'eventLabel': 'pinterest pin',
+            'eventValue': 1
+        });
+    });
+
+    $('.external-site a').on('click', function(){
+        ga('send', {
+            'hitType': 'event',
+            'eventCategory': 'button',
+            'eventAction': 'click',
+            'eventLabel': $(this).attr('title'),
+            'eventValue': 1
+        });
+    });
+
+    $(window).on("scroll", function() {
+        var scrollHeight = $(document).height();
+        var scrollPosition = $(window).height() + $(window).scrollTop();
+        if ((scrollHeight - scrollPosition) / scrollHeight === 0) {
+            ga('send', {
+                'hitType': 'event',
+                'eventCategory': 'post',
+                'eventAction': 'read',
+                'eventLabel': 'post read',
+                'eventValue': 1
+            });
+        }
+    });
 });
