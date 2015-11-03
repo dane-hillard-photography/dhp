0a66c2757dc194879782ad48377104173be13d9c
Dane Hillard <github@danehillard.com>
2015-11-03 17:11:42 -0500

    Add contact tracking

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,11 +1,17 @@
 $(function(){
-    $('.send-button').on('click', function(){
-        ga('send', {
-            'hitType': 'event',
-            'eventCategory': 'button',
-            'eventAction': 'click',
-            'eventLabel': 'send message',
-            'eventValue': 1
-        });
+    $('.send-button').on('click', function(e){
+        e.preventDefault();
+        ga(
+            'send',
+            {
+                'hitType': 'event',
+                'eventCategory': 'button',
+                'eventAction': 'click',
+                'eventLabel': 'send message',
+                'eventValue': 1,
+                hitCallback: function() {
+                    $('.send-button').parents('form:first').submit();
+                }
+            });
     });
 });
