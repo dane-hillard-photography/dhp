79ca40adb526b853a15e6e07b4a6271223ed3ed3
Dane Hillard <github@danehillard.com>
2015-11-10 18:23:16 -0500

    Update to new Disqus universal code

--- templates/disqus.js
+++ templates/disqus.js
@@ -1,6 +1,6 @@
-var disqus_shortname = 'danehillard';
 (function() {
-    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    var d = document, s = d.createElement('script');
+    s.src = '//danehillard.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
 })();
