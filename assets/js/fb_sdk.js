2cad65fc537c2f2d7494c8324bf815321f39619e
Dane Hillard <github@danehillard.com>
2015-11-11 09:08:28 -0500

    Add version specifier to facebook SDK JS

--- assets/js/fb_sdk.js
+++ assets/js/fb_sdk.js
@@ -2,6 +2,6 @@
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=152446884139";
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&version=v2.5&appId=152446884139";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
