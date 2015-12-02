502d0c479e60216edbe30183113b609d7f15b97c
Dane Hillard <github@danehillard.com>
2015-12-02 14:27:05 -0500

    Upgrade to Django 1.9

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,9 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from blog import views
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^post/(?P<slug>[\w\-]+)/$', views.post_view, name='post'),
     url(r'^archive/(?P<year>[0-9]{4})/$', views.PostYearArchiveView.as_view(), name='post_year_archive'),
     url(r'^archive/(?P<year>[0-9]{4})/(?P<month>0[1-9]|1[012])/$', views.PostMonthArchiveView.as_view(), name='post_month_archive'),
-)
+]
