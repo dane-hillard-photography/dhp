04d5dc54a7f0bd2afdfff4fccee2dfb623714ce7
Dane Hillard <github@danehillard.com>
2016-01-12 14:59:03 -0500

    Integrate Mailchimp API

--- contact/views.py
+++ contact/views.py
@@ -1,12 +1,16 @@
+import json
+import hashlib
 import logging
 
+from django.conf import settings
 from django.views import generic
 from django.template import Context, loader
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic.edit import FormView
 
 from boto import ses
-from django.conf import settings
+
+import requests
 
 from contact.forms import ContactForm
 
@@ -57,6 +61,37 @@ class ContactFormView(FormView):
             to_addresses=contact_email,
             format='html'
         )
+
+        if form.cleaned_data.get('join_mailing_list'):
+            subscription_body = {
+                'email_address': contact_email,
+                'status': 'subscribed',
+                'merge_fields': {
+                    'FNAME': contact_first_name,
+                    'LNAME': contact_last_name,
+                }
+            }
+
+            subscription_response = requests.post(
+                'https://us3.api.mailchimp.com/3.0/lists/{}/members'.format(settings.MAILCHIMP_LIST_ID),
+                data=json.dumps(subscription_body),
+                auth=('foo', settings.MAILCHIMP_API_KEY)
+            )
+
+            if subscription_response.status_code == 200:
+                md5 = hashlib.md5()
+                md5.update(contact_email.encode('utf-8'))
+                member_id = md5.hexdigest()
+
+                check = requests.get('https://us3.api.mailchimp.com/3.0/lists/{}/members/{}'.format(
+                    settings.MAILCHIMP_LIST_ID,
+                    member_id
+                ))
+
+                if check.status_code != 200:
+                    logging.error('Subscription call was successful, but member \'{}\' does not appear to exist'.format(contact_email))
+            else:
+                logging.error('Unable to subscribe member \'{}\': {}'.format(contact_email, subscription_response.text))
 
         return super(ContactFormView, self).form_valid(form)
 
