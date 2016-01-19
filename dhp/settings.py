83bab0fc79624a67e4ef2fbb42e58ad1aeafae18
Dane Hillard <github@danehillard.com>
2016-01-19 11:42:46 -0500

    Use latest definition of BASE_DIR to avoid confusion

--- dhp/settings.py
+++ dhp/settings.py
@@ -1,6 +1,6 @@
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 PROJECT_NAME = 'DHP'
 PROJECT_VARIABLE_PATTERN = '_'.join((PROJECT_NAME, '{}'))
