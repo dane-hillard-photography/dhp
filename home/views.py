cc618dc4cf76a077fdae0d6e42ecb11a9c96db6a
Dane Hillard <github@danehillard.com>
2016-02-08 22:32:11 -0500

    Make space for more posts on the home page

--- home/views.py
+++ home/views.py
@@ -11,5 +11,6 @@ class IndexView(generic.TemplateView):
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
         right_now = datetime.now()
-        context['latest_posts'] = Post.objects.filter(go_live_date__lte=right_now).exclude(take_down_date__lte=right_now).order_by('-go_live_date')[:3]
+        context['latest_posts'] = Post.objects.filter(go_live_date__lte=right_now).exclude(take_down_date__lte=right_now).order_by('-go_live_date')[:5]
+
         return context
