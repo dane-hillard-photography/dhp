a47e27ca93145a1cd2ba3f823621dd7e9d10aa2e
Dane Hillard <github@danehillard.com>
2016-05-15 16:39:40 -0400

    Add more unit tests

---
+++ tests/test_context_processors.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+from unittest.mock import Mock
+
+from django.test import override_settings
+
+from context_processors import template_visible_settings
+
+
+class ContextProcessorTestCase(TestCase):
+    @override_settings(TEMPLATE_VISIBLE_SETTINGS=('FOO',), FOO='bar')
+    def test_template_visible_settings(self):
+        request = Mock()
+        self.assertDictEqual({'FOO': 'bar'}, template_visible_settings(request))
