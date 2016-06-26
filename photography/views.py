125038a69b92b7d987a879fb88be0caeb2a7133f
Dane Hillard <github@danehillard.com>
2016-06-26 14:30:31 -0400

    Order portfolio by most recently added first

--- photography/views.py
+++ photography/views.py
@@ -8,5 +8,5 @@ class PortfolioView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
-        context['photos'] = Photograph.objects.filter(in_portfolio=True)
+        context['photos'] = Photograph.objects.filter(in_portfolio=True).order_by('-pk')
         return context
