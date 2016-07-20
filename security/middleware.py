321eb11b26cffc4b855130a900d28621cc616bb6
Dane Hillard <github@danehillard.com>
2016-07-20 16:08:04 -0400

    Make CSP strings more readable

--- security/middleware.py
+++ security/middleware.py
@@ -1,10 +1,10 @@
 CONTENT_SECURITY_POLICY = {
-    'default-src': '\'self\'',
-    'script-src': '\'self\' connect.facebook.net assets.pinterest.com danehillard.disqus.com danehillard-dev.disqus.com log.pinterest.com \'unsafe-inline\'',
-    'style-src': '\'self\' fonts.googleapis.com a.disquscdn.com \'unsafe-inline\'',
-    'font-src': '\'self\' fonts.gstatic.com',
-    'frame-src': '\'self\' staticxx.facebook.com www.facebook.com disqus.com',
-    'img-src': '\'self\' data: www.facebook.com referrer.disqus.com a.disquscdn.com',
+    'default-src': "'self'",
+    'script-src': "'self' connect.facebook.net assets.pinterest.com danehillard.disqus.com danehillard-dev.disqus.com log.pinterest.com 'unsafe-inline'",
+    'style-src': "'self' fonts.googleapis.com a.disquscdn.com 'unsafe-inline'",
+    'font-src': "'self' fonts.gstatic.com",
+    'frame-src': "'self' staticxx.facebook.com www.facebook.com disqus.com",
+    'img-src': "'self' data: www.facebook.com referrer.disqus.com a.disquscdn.com",
 }
 
 class ContentSecurityPolicyMiddleware(object):
