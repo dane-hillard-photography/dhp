b6afe81be396131d5b280ccbfc48eb5e1ecc9592
Dane Hillard <github@danehillard.com>
2016-09-30 17:38:35 -0400

    Remove unneeded tracking of page elements

--- assets/js/blog-post.js
+++ assets/js/blog-post.js
@@ -1,24 +1,4 @@
 $(function(){
-    $('#feedlyFollow').on('click', function(){
-        ga('send', {
-            'hitType': 'event',
-            'eventCategory': 'button',
-            'eventAction': 'click',
-            'eventLabel': 'feedly follow',
-            'eventValue': 1
-        });
-    });
-
-    $('#pinterest-pin').on('click', function(){
-        ga('send', {
-            'hitType': 'event',
-            'eventCategory': 'button',
-            'eventAction': 'click',
-            'eventLabel': 'pinterest pin',
-            'eventValue': 1
-        });
-    });
-
     $('.external-site a').on('click', function(){
         ga('send', {
             'hitType': 'event',
