0cdbf4c365f41618f123b157c2720c03844396f8
Dane Hillard <github@danehillard.com>
2016-11-01 16:50:53 -0400

    Add instagram to CSP

--- security/middleware.py
+++ security/middleware.py
@@ -3,7 +3,7 @@ CONTENT_SECURITY_POLICY = {
     'script-src': "'self' connect.facebook.net assets.pinterest.com danehillard.disqus.com danehillard-dev.disqus.com log.pinterest.com www.google-analytics.com platform.instagram.com 'unsafe-inline'",
     'style-src': "'self' fonts.googleapis.com a.disquscdn.com 'unsafe-inline'",
     'font-src': "'self' fonts.gstatic.com",
-    'frame-src': "'self' staticxx.facebook.com www.facebook.com disqus.com",
+    'frame-src': "'self' staticxx.facebook.com www.facebook.com www.instagram.com disqus.com",
     'img-src': "'self' data: www.facebook.com referrer.disqus.com a.disquscdn.com www.google-analytics.com",
 }
 
