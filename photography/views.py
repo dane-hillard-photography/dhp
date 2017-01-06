bfcbe58010f640886b1cc51f3fd5d5074d2e9fbf
Dane Hillard <github@danehillard.com>
2017-01-06 10:18:08 -0500

    Fix formatting

--- photography/views.py
+++ photography/views.py
@@ -11,5 +11,6 @@ class PortfolioView(TemplateView):
         context['photos'] = Photograph.objects.filter(in_portfolio=True).order_by('-pk')
         return context
 
+
 def do_nothing(request):
-    return ('foo'+ 'bar')
+    return 'foo' + 'bar'
