6bf223c666ff6f325dcf7f5aa56b084aa3a2f68a
Dane Hillard <github@danehillard.com>
2017-04-01 19:00:31 -0400

    Update Google Optimize container ID

--- assets/js/google_analytics_tracking_flicker.js
+++ assets/js/google_analytics_tracking_flicker.js
@@ -2,4 +2,4 @@
 h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
 (a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
 })(window,document.documentElement,'async-hide','dataLayer',4000,
-{'GTM-5CC7GNV':true});
+{'GTM-NRQGXQ9':true});
