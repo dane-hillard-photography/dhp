bc4622892d3740b9f28790cc0e657f5780ff809a
Dane Hillard <github@danehillard.com>
2017-04-01 18:37:31 -0400

    Add Google Optimize code for trial run

--- assets/js/google_analytics_tracking.js
+++ assets/js/google_analytics_tracking.js
@@ -4,4 +4,5 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-12156455-2', 'auto');
+ga('require', 'GTM-5CC7GNV');
 ga('send', 'pageview');
