d28c68ca8d4b6d22332cd897d16b9c9fe8f9780c
Dane Hillard <github@danehillard.com>
2017-12-15 21:24:56 -0500

    Update deploy script for pipenv

--- deploy.zsh
+++ deploy.zsh
@@ -17,12 +17,13 @@ ENVIRONMENT=production
 LOCAL_USERNAME=`whoami`
 REVISION=`git log -n 1 --pretty=format:"%H"`
 
-source .env
+pipenv shell
 curl https://api.rollbar.com/api/1/deploy/ \
   -F access_token=$DHP_ROLLBAR_ACCESS_TOKEN \
   -F environment=$ENVIRONMENT \
   -F revision=$REVISION \
   -F local_username=$LOCAL_USERNAME
+exit
 
 cd -
 echo 'flush_all' | nc localhost 11211
